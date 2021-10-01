08aa381746944d97d96980d86a25d064009549a5
PtOlemy01 <shapewki@naver.com>
2021-10-01 16:13:49 +0900

    pizza 예제.

---
+++ src/Chapter2/item2/OrderPizza.java
@@ -0,0 +1,11 @@
+package Chapter2.item2;
+
+public class OrderPizza {
+    public static void main(String[] args) {
+        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE).addTopping(Pizza.Topping.ONION).build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM).sauceInside().build();
+    }
+}
