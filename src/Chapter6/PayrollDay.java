ac976fbbd49d8b4bd78446b461c169d9bd8832a2
PtOlemy01 <shapewki@naver.com>
2021-10-28 10:51:33 +0900

    전략 열거 타입 패턴

---
+++ src/Chapter6/PayrollDay.java
@@ -0,0 +1,39 @@
+package Chapter6;
+
+import static Chapter6.PayrollDay.PayType.WEEKDAY;
+import static Chapter6.PayrollDay.PayType.WEEKEND;
+
+public enum PayrollDay {
+    MONDAY(WEEKDAY), TUESDAY(WEEKDAY), WEDNESDAY(WEEKDAY),
+    THURSDAY(WEEKDAY), FRIDAY(WEEKDAY),
+    SATURDAY(WEEKEND), SUNDAY(WEEKEND);
+
+    private final PayType  payType;
+
+    PayrollDay(PayType payType) { this.payType = payType;}
+
+    int pay(int minuteWorked, int payRate){
+        return payType.pay(minuteWorked, payRate);
+    }
+
+    enum PayType{
+        WEEKDAY{
+            int overtimePay(int minsWorked, int payRate){
+                return minsWorked <= MINS_PER_SHIFT ? 0 : (minsWorked - MINS_PER_SHIFT) * payRate /2;
+            }
+        },
+        WEEKEND{
+            int overtimePay(int minsWorked, int payRate){
+                return minsWorked * payRate /2;
+            }
+        };
+
+        abstract int overtimePay(int mins, int payRate);
+        private static final int MINS_PER_SHIFT = 8 *60;
+
+        int pay(int minsWorked, int payRate){
+            int basePay = minsWorked * payRate;
+            return basePay + overtimePay(minsWorked, payRate);
+        }
+    }
+}
