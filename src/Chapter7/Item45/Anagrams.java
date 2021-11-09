59fee197b66744f90ecb2b1dd37cb464733d8e51
PtOlemy01 <shapewki@naver.com>
2021-11-09 13:10:21 +0900

    스트림을 적절히 사용.

--- src/Chapter7/Item45/Anagrams.java
+++ src/Chapter7/Item45/Anagrams.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -14,15 +15,16 @@ public class Anagrams {
         int minGroupSize = Integer.parseInt(args[1]);
 
         try(Stream<String> words = Files.lines(dictionary)){
-            words.collect(
-                    groupingBy(word -> word.chars().sorted()
-                            .collect(StringBuilder::new,
-                                    (sb, c) -> sb.append((char) c),
-                                    StringBuilder::append).toString()))
+            words.collect( groupingBy(word -> alphabetize(word)))
             .values().stream()
             .filter(group -> group.size() >= minGroupSize)
-            .map(group -> group.size() + " : " + group)
-            .forEach(System.out::println);
+            .forEach(g -> System.out.println(g.size() + " : " + g ));
         }
+    }
+
+    private static String alphabetize(String s){
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
     }
 }
